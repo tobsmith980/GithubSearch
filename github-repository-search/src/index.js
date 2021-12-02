46dac194ffbb3aa6a700d2150856e87be7c1ff40
Tobias Smith <tobsmith96@gmail.com>
2021-12-02 03:07:10 -0500

    Basic Record Functionality Added

--- github-repository-search/src/index.js
+++ github-repository-search/src/index.js
@@ -2,8 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class List extends React.Component {
+class Result extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <rect className="Rectangle">{this.props.id}</rect>
+                <rect className="Rectangle">{this.props.name}</rect>
+                <rect className="Rectangle">{this.props.owner}</rect>
+            </div>
+        )
+    }
     
+}
+
+class List extends React.Component {
+    renderRecord(data) {
+        return (
+            <Result
+                id={data.id}
+                name={data.name}
+                owner={data.owner}
+            />
+        )
+    }
+    // First Render what each of the columns will be on top
+    // The top will also be a record with the names of each field
+    // After that it will just be records with data
+    // For each result, render a new record for that result
+
+    render() {
+        const headers = {
+            id : "ID",
+            name : "NAME",
+            owner : "OWNER",
+        }
+
+        const headers1 = {
+            id : "ID1",
+            name : "NAME",
+            owner : "OWNER",
+        }
+
+        const headers2 = {
+            id : "ID2",
+            name : "NAME",
+            owner : "OWNER",
+        }
+
+        return (
+            <div>
+                {this.renderRecord(headers)}
+                {this.renderRecord(headers1)}
+                {this.renderRecord(headers2)}
+            </div>
+        );
+    }
 }
 
 class Application extends React.Component {
@@ -32,11 +85,16 @@ class Application extends React.Component {
             <div className="App">
                 <button onClick={() => this.toggleRepoSearch(true)}>Repo Search</button>
                 <button onClick={() => this.toggleRepoSearch(false)}>View Commits</button>
+                <br/>
+                <br/>
                 <div className="List">
-
+                    <List 
+                        />
                 </div>
             </div>
 
         );
     }
 }
+
+ReactDOM.render(<Application />, document.getElementById("root"));
