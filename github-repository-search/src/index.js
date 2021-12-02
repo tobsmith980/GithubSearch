d3af81d57545ee22d9de0e3ea6b96cf80c5cf10b
Tobias Smith <tobsmith96@gmail.com>
2021-12-01 23:20:02 -0500

    Updated index.js with some React Components

--- github-repository-search/src/index.js
+++ github-repository-search/src/index.js
@@ -2,3 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class List extends React.Component {
+    
+}
+
+class Application extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            repo  : null,
+            owner : null,
+            keyword : null,
+            repoSearch: true,
+            data    : [
+                {
+                }
+            ]
+        }
+    }
+
+    toggleRepoSearch(bool) {
+        this.setState({
+            repoSearch : bool
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={() => this.toggleRepoSearch(true)}>Repo Search</button>
+                <button onClick={() => this.toggleRepoSearch(false)}>View Commits</button>
+                <div className="List">
+
+                </div>
+            </div>
+
+        );
+    }
+}
